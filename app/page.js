d72d75580525a788b718566a46b190ca71e23eb9
T.S. Hayagriva Kishan <sharmilakishan.2003@gmail.com>
2025-02-24 08:44:00 +0530

    navbar added

--- app/page.js
+++ app/page.js
@@ -1,10 +1,8 @@
 import { Button } from "@/components/ui/button";
-import { Accordion } from "@radix-ui/react-accordion";
-import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex flex-col justify-center items-center mt-[50%]">
+    <div>
       <div>Welcome to mentorX</div>
       <Button className>Click me</Button>
     </div>
--- components/header.jsx
+++ components/header.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+import Image from "next/image";
+import Link from "next/link";
+import { Button } from "./ui/button";
+import { LayoutDashboard } from "lucide-react";
 
 const Header = () => {
-    return (
-        <div>header</div>
-    )
-}
+  return (
+    <header className="fixed top-1 w-full border-b bg-background/80 backdrop-blur-md z-50 supports-[backdrop-filter]:bg-background/60">
+      <nav className="container mt-4 mx-auto px-4 h-16 flex items-center justify-between">
+        <Link href="/">
+          <Image
+            src="/logo-transparent.png"
+            alt="logo"
+            width={65}
+            height={60}
+            className="h-13 py-1 w-auto object-contain"
+          />
+        </Link>
 
-export default Header
+        <div>
+          <SignedIn>
+            <Link href={"./dashboard"}>
+              <Button>
+                <LayoutDashboard className="h-4 w-4" />
+                Industry Insights
+              </Button>
+            </Link>
+          </SignedIn>
+        </div>
+      </nav>
+
+      <SignedOut>
+        <SignInButton />
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+    </header>
+  );
+};
+
+export default Header;
