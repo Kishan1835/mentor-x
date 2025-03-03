e192e099333a4f56f956b03b53f83166ee861826
T.S. Hayagriva Kishan <sharmilakishan.2003@gmail.com>
2025-03-03 23:54:56 +0530

    Refactor auth layout and enhance global styles.

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,20 +1,25 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { ThemeProvider } from "@/components/theme-provider";
-import Header from "@/components/header";
 import { ClerkProvider } from "@clerk/nextjs";
+import Header from "@/components/header";
+import { ThemeProvider } from "@/components/theme-provider";
+import { dark } from "@clerk/themes";
+
 
 const inter = Inter({ subset: ["latin"] });
 
-
 export const metadata = {
-  title: " Mentor-X    AI-driven career mentorship ",
+  title: " Mentor-X  AI-driven career mentorship ",
   description: "",
 };
 
 export default function RootLayout({ children }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider
+      appearance={{
+        baseTheme: dark,
+      }}
+    >
       <html lang="en" suppressHydrationWarning>
         <body className={`${inter.className}`}>
           <ThemeProvider
