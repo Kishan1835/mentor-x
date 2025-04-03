9de853d5b7e9077ba4303886569a4ea1813fe9d0
T.S. Hayagriva Kishan <sharmilakishan.2003@gmail.com>
2025-04-03 23:42:08 +0530

    Last Updated badge in DashboardView component.

--- app/(main)/dashboard/_components/dashboard-View.jsx
+++ app/(main)/dashboard/_components/dashboard-View.jsx
@@ -1,13 +1,21 @@
 "use client"
 
 import React from 'react'
+import { format, formatDistanceToNow } from "date-fns"
+import { Badge } from '@/components/ui/badge'
 import {
     LineChart,
     TrendingDown,
     TrendingUp
 } from 'lucide-react'
-import { format, formatDistanceToNow } from "date-fns"
-import { Badge } from '@/components/ui/badge'
+import {
+    Card,
+    CardContent,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card"
+import { OutdentIcon } from 'lucide-react'
+
 
 export const DashboardView = ({ insights }) => {
 
@@ -49,8 +57,8 @@ export const DashboardView = ({ insights }) => {
         }
     }
 
-    const outlookIcon = getMarketOutlookInfo(insights.marketoutlook).icon;
-    const outlookColor = getMarketOutlookInfo(insights.marketoutlook).color
+    const OutlookIcon = getMarketOutlookInfo(insights.marketOutlook).icon;
+    const OutlookColor = getMarketOutlookInfo(insights.marketOutlook).color
 
     // it will show when it was last updated
     const lastUpdatedDate = insights.lastUpdated ? format(new Date(insights.lastUpdated), "dd/MM/yyyy") : "N/A";
@@ -64,6 +72,55 @@ export const DashboardView = ({ insights }) => {
         <div className='flex justify-between items-center'>
             <Badge variant="outline">Last Updated: {lastUpdatedDate}</Badge>
         </div>
+
+        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4'>
+            <Card>
+                <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                    <CardTitle className="text-sm font-medium">Market Outlook</CardTitle>
+                    <OutlookIcon className={`h-4 w-4${OutlookColor}`} />
+                </CardHeader>
+                <CardContent>
+                    <div className='text-2xl font-bold'>{insights.marketOutlook}
+                    </div>
+                    <p className='text-xs text-muted-foreground'>Next Update {nextUpdateDistance}</p>
+                </CardContent>
+            </Card>
+            <Card>
+                <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                    <CardTitle className="text-sm font-medium">Market Outlook</CardTitle>
+                    <OutlookIcon className={`h-4 w-4${OutlookColor}`} />
+                </CardHeader>
+                <CardContent>
+                    <div className='text-2xl font-bold'>{insights.marketOutlook}
+                    </div>
+                    <p className='text-xs text-muted-foreground'>Next Update {nextUpdateDistance}</p>
+                </CardContent>
+            </Card>
+            <Card>
+                <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                    <CardTitle className="text-sm font-medium">Market Outlook</CardTitle>
+                    <OutlookIcon className={`h-4 w-4${OutlookColor}`} />
+                </CardHeader>
+                <CardContent>
+                    <div className='text-2xl font-bold'>{insights.marketOutlook}
+                    </div>
+                    <p className='text-xs text-muted-foreground'>Next Update {nextUpdateDistance}</p>
+                </CardContent>
+            </Card>
+            <Card>
+                <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                    <CardTitle className="text-sm font-medium">Market Outlook</CardTitle>
+                    <OutlookIcon className={`h-4 w-4${OutlookColor}`} />
+                </CardHeader>
+                <CardContent>
+                    <div className='text-2xl font-bold'>{insights.marketOutlook}
+                    </div>
+                    <p className='text-xs text-muted-foreground'>Next Update {nextUpdateDistance}</p>
+                </CardContent>
+            </Card>
+
+        </div>
+
     </div>
 
 
